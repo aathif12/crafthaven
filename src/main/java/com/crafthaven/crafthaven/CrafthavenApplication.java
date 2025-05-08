b40716d73c84f07da8e696dfb32b7660a8dc8530
aathif12 <aathifahamad4@gmail.com>
2025-05-08 15:10:05 +0530

    Initail Commit

---
+++ src/main/java/com/crafthaven/crafthaven/CrafthavenApplication.java
@@ -0,0 +1,13 @@
+package com.crafthaven.crafthaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrafthavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrafthavenApplication.class, args);
+	}
+
+}
