d79212d2a039ee7b3fd2a62c65e466e3b2d9060e
aathif12 <aathifahamad4@gmail.com>
2025-06-17 19:13:45 +0530

    Creating Models

---
+++ src/main/com/carfthaven/models/OrderItem.java
@@ -0,0 +1,17 @@
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantity;
+    private Double price;
+}
---
+++ src/main/com/carfthaven/models/Product.java
@@ -0,0 +1,26 @@
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private Double price;
+    private Integer stock;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User seller;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    private Double rating = 0.0;
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    // List of product images will be separate table
+}
